562346f45ce3d91fc296efd1d6088aee7387d2ff
Rockey Ahmed <rockeycse@gmail.com>
2021-06-15 09:40:19 +0600

    has review error problem solved

--- public/js/app.js
+++ public/js/app.js
@@ -2345,6 +2345,19 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   data: function data() {
     return {
@@ -2367,7 +2380,7 @@ __webpack_require__.r(__webpack_exports__);
 
     this.loading = true;
     axios.get("/api/reviews/".concat(this.$route.params.id)).then(function (response) {
-      _this.existingReview = response.data.data;
+      _this.existingReview = response.data.data; // console.log(response.data.data);
     })["catch"](function (err) {
       if (err.response && err.response.status && 404 == err.response.status) {
         return axios.get("/api/booking-by-review/".concat(_this.$route.params.id)).then(function (response) {
@@ -2382,12 +2395,14 @@ __webpack_require__.r(__webpack_exports__);
   },
   computed: {
     alreadyReviewed: function alreadyReviewed() {
-      return this.hasReview || this.hasBooking != null;
+      return this.hasReview || !this.booking; // return false;
     },
     hasReview: function hasReview() {
+      //console.log("existingReview " +this.existingReview);
       return this.existingReview != null;
     },
     hasBooking: function hasBooking() {
+      // console.log("has booking "+this.booking);
       return this.booking != null;
     }
   }
@@ -60761,82 +60776,145 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div", [
-    _vm.loading
-      ? _c("div", [_vm._v("Loading...")])
-      : _c("div", [
-          _vm.alreadyReviewed
-            ? _c("div", [
-                _c("h3", [
-                  _vm._v("You have already left a review for this booking")
-                ])
-              ])
-            : _c("div", [
-                _c(
-                  "div",
-                  { staticClass: "form-group" },
-                  [
-                    _c(
-                      "label",
-                      { staticClass: "text-muted", attrs: { for: "" } },
-                      [
-                        _vm._v(
-                          "\n          Select the star rating (1 is worst - 5 is best)"
-                        )
-                      ]
-                    ),
-                    _vm._v(" "),
-                    _c("star-rating", {
-                      staticClass: "fa-3x",
-                      model: {
-                        value: _vm.review.rating,
-                        callback: function($$v) {
-                          _vm.$set(_vm.review, "rating", $$v)
-                        },
-                        expression: "review.rating"
-                      }
-                    })
-                  ],
-                  1
-                ),
-                _vm._v(" "),
-                _c("div", { staticClass: "form-group" }, [
+  return _c("div", { staticClass: "row" }, [
+    _c(
+      "div",
+      {
+        class: [
+          { "col-md-4": _vm.loading || !_vm.alreadyReviewed },
+          { "d-none": !_vm.loading && _vm.alreadyReviewed }
+        ]
+      },
+      [
+        _c("div", { staticClass: "card" }, [
+          _c("div", { staticClass: "card-body" }, [
+            _vm.loading
+              ? _c("div", [_vm._v("Loading...")])
+              : _c("div", [
                   _c(
-                    "label",
-                    { staticClass: "text-muted", attrs: { for: "content" } },
-                    [_vm._v("Describe your experience with\n        ")]
+                    "p",
+                    [
+                      _vm._v(" Stayed at "),
+                      _c(
+                        "router-link",
+                        {
+                          attrs: {
+                            to: {
+                              name: "bookable",
+                              params: { id: _vm.booking.bookable.bookable_id }
+                            }
+                          }
+                        },
+                        [_vm._v(" " + _vm._s(_vm.booking.bookable.title) + " ")]
+                      )
+                    ],
+                    1
                   ),
                   _vm._v(" "),
-                  _c("textarea", {
-                    directives: [
-                      {
-                        name: "model",
-                        rawName: "v-model",
-                        value: _vm.review.content,
-                        expression: "review.content"
-                      }
-                    ],
-                    staticClass: "form-control",
-                    attrs: { name: "content", cols: "30", rows: "10" },
-                    domProps: { value: _vm.review.content },
-                    on: {
-                      input: function($event) {
-                        if ($event.target.composing) {
-                          return
-                        }
-                        _vm.$set(_vm.review, "content", $event.target.value)
-                      }
-                    }
-                  })
-                ]),
-                _vm._v(" "),
-                _c(
-                  "button",
-                  { staticClass: "btn btn-lg- btn-primary btn-block" },
-                  [_vm._v("Submit")]
-                )
-              ])
+                  _c("p", [
+                    _vm._v(
+                      "From " +
+                        _vm._s(_vm.booking.from) +
+                        " to " +
+                        _vm._s(_vm.booking.to) +
+                        " "
+                    )
+                  ])
+                ])
+          ])
         ])
+      ]
+    ),
+    _vm._v(" "),
+    _c(
+      "div",
+      {
+        class: [
+          { "col-md-8": _vm.loading || !_vm.alreadyReviewed },
+          { "col-md-12": !_vm.loading && _vm.alreadyReviewed }
+        ]
+      },
+      [
+        _vm.loading
+          ? _c("div", [_vm._v("Loading...")])
+          : _c("div", [
+              _vm.alreadyReviewed
+                ? _c("div", [
+                    _c("h3", [
+                      _vm._v("You have already left a review for this booking")
+                    ])
+                  ])
+                : _c("div", [
+                    _c(
+                      "div",
+                      { staticClass: "form-group" },
+                      [
+                        _c(
+                          "label",
+                          { staticClass: "text-muted", attrs: { for: "" } },
+                          [
+                            _vm._v(
+                              "\n            Select the star rating (1 is worst - 5 is best)"
+                            )
+                          ]
+                        ),
+                        _vm._v(" "),
+                        _c("star-rating", {
+                          staticClass: "fa-3x",
+                          model: {
+                            value: _vm.review.rating,
+                            callback: function($$v) {
+                              _vm.$set(_vm.review, "rating", $$v)
+                            },
+                            expression: "review.rating"
+                          }
+                        })
+                      ],
+                      1
+                    ),
+                    _vm._v(" "),
+                    _c("div", { staticClass: "form-group" }, [
+                      _c(
+                        "label",
+                        {
+                          staticClass: "text-muted",
+                          attrs: { for: "content" }
+                        },
+                        [_vm._v("Describe your experience with\n          ")]
+                      ),
+                      _vm._v(" "),
+                      _c("textarea", {
+                        directives: [
+                          {
+                            name: "model",
+                            rawName: "v-model",
+                            value: _vm.review.content,
+                            expression: "review.content"
+                          }
+                        ],
+                        staticClass: "form-control",
+                        attrs: { name: "content", cols: "30", rows: "10" },
+                        domProps: { value: _vm.review.content },
+                        on: {
+                          input: function($event) {
+                            if ($event.target.composing) {
+                              return
+                            }
+                            _vm.$set(_vm.review, "content", $event.target.value)
+                          }
+                        }
+                      })
+                    ]),
+                    _vm._v(" "),
+                    _c(
+                      "button",
+                      { staticClass: "btn btn-lg- btn-primary btn-block" },
+                      [_vm._v("Submit")]
+                    )
+                  ])
+            ])
+      ]
+    )
   ])
 }
 var staticRenderFns = []
--- resources/js/review/Review.vue
+++ resources/js/review/Review.vue
@@ -1,30 +1,43 @@
 <template>
-  <div>
-    <div v-if="loading">Loading...</div>
-    <div v-else>
-      <div v-if="alreadyReviewed">
-        <h3>You have already left a review for this booking</h3>
+  <div class="row">
+    <div :class="[{'col-md-4': loading || !alreadyReviewed}, {'d-none': !loading && alreadyReviewed}]">
+      <div class="card">
+        <div class="card-body">
+          <div v-if="loading">Loading...</div>
+          <div v-else>
+            <p> Stayed at <router-link :to="{name: 'bookable', params:{ id: booking.bookable.bookable_id}}"> {{ booking.bookable.title }} </router-link></p>
+            <p>From {{ booking.from }} to {{ booking.to }} </p>
+          </div>
+        </div>
       </div>
+    </div>
+    <div :class="[{'col-md-8': loading || !alreadyReviewed}, {'col-md-12': !loading && alreadyReviewed}]">
+      <div v-if="loading">Loading...</div>
       <div v-else>
-        <div class="form-group">
-          <label for="" class="text-muted">
-            Select the star rating (1 is worst - 5 is best)</label
-          >
-          <star-rating class="fa-3x" v-model="review.rating"></star-rating>
+        <div v-if="alreadyReviewed">
+          <h3>You have already left a review for this booking</h3>
         </div>
-        <div class="form-group">
-          <label for="content" class="text-muted"
-            >Describe your experience with
-          </label>
-          <textarea
-            name="content"
-            cols="30"
-            rows="10"
-            class="form-control"
-            v-model="review.content"
-          ></textarea>
+        <div v-else>
+          <div class="form-group">
+            <label for="" class="text-muted">
+              Select the star rating (1 is worst - 5 is best)</label
+            >
+            <star-rating class="fa-3x" v-model="review.rating"></star-rating>
+          </div>
+          <div class="form-group">
+            <label for="content" class="text-muted"
+              >Describe your experience with
+            </label>
+            <textarea
+              name="content"
+              cols="30"
+              rows="10"
+              class="form-control"
+              v-model="review.content"
+            ></textarea>
+          </div>
+          <button class="btn btn-lg- btn-primary btn-block">Submit</button>
         </div>
-        <button class="btn btn-lg- btn-primary btn-block">Submit</button>
       </div>
     </div>
   </div>
@@ -53,14 +66,15 @@ export default {
 
     axios
       .get(`/api/reviews/${this.$route.params.id}`)
-      .then((response) => {
+      .then( response => {
         this.existingReview = response.data.data;
+        // console.log(response.data.data);
       })
-      .catch((err) => {
-        if (err.response && err.response.status && 404 == err.response.status) {
+      .catch( err => {
+        if ( err.response && err.response.status && 404 == err.response.status) {
           return axios
             .get(`/api/booking-by-review/${this.$route.params.id}`)
-            .then((response) => {
+            .then( response => {
               this.booking = response.data.data;
             });
         }
@@ -73,14 +87,17 @@ export default {
   },
   computed: {
     alreadyReviewed() {
-      return this.hasReview || this.hasBooking != null;
+      return this.hasReview || !this.booking;
+      // return false;
     },
-    hasReview(){
+    hasReview() {
+      //console.log("existingReview " +this.existingReview);
       return this.existingReview != null;
     },
-    hasBooking(){
+    hasBooking() {
+      // console.log("has booking "+this.booking);
       return this.booking != null;
-    }
+    },
   },
 };
 </script>
