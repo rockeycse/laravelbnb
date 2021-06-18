0358bfc9c9cdc52e9caeb789350fb34b97fd4919
Rockey Ahmed <rockeycse@gmail.com>
2021-06-18 09:48:23 +0600

    review controller

--- app/Review.php
+++ app/Review.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Review extends Model
 {
+    protected $fillable = ['id', 'content', 'rating'];
+
     public function bookable()
     {
         return $this->belongsTo(Bookable::class);
--- resources/js/review/Review.vue
+++ resources/js/review/Review.vue
@@ -1,42 +1,63 @@
 <template>
-  <div class="row">
-    <div :class="[{'col-md-4': loading || !alreadyReviewed}, {'d-none': !loading && alreadyReviewed}]">
-      <div class="card">
-        <div class="card-body">
-          <div v-if="loading">Loading...</div>
-          <div v-else>
-            <p> Stayed at <router-link :to="{name: 'bookable', params:{ id: booking.bookable.bookable_id}}"> {{ booking.bookable.title }} </router-link></p>
-            <p>From {{ booking.from }} to {{ booking.to }} </p>
+  <div>
+    <div class="row" v-if="error">
+      Unknown error has occured, please try again later!
+    </div>
+    <div class="row" v-else>
+      <div :class="[{ 'col-md-4': twoColumns }, { 'd-none': oneColumn }]">
+        <div class="card">
+          <div class="card-body">
+            <div v-if="loading">Loading...</div>
+            <div v-if="hasBooking">
+              <p>
+                Stayed at
+                <router-link
+                  :to="{
+                    name: 'bookable',
+                    params: { id: booking.bookable.bookable_id },
+                  }"
+                >
+                  {{ booking.bookable.title }}
+                </router-link>
+              </p>
+              <p>From {{ booking.from }} to {{ booking.to }}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div :class="[{'col-md-8': loading || !alreadyReviewed}, {'col-md-12': !loading && alreadyReviewed}]">
-      <div v-if="loading">Loading...</div>
-      <div v-else>
-        <div v-if="alreadyReviewed">
-          <h3>You have already left a review for this booking</h3>
-        </div>
+      <div :class="[{ 'col-md-8': twoColumns }, { 'col-md-12': oneColumn }]">
+        <div v-if="loading">Loading...</div>
         <div v-else>
-          <div class="form-group">
-            <label for="" class="text-muted">
-              Select the star rating (1 is worst - 5 is best)</label
+          <div v-if="alreadyReviewed">
+            <h3>You have already left a review for this booking</h3>
+          </div>
+          <div v-else>
+            <div class="form-group">
+              <label for="" class="text-muted">
+                Select the star rating (1 is worst - 5 is best)</label
+              >
+              <star-rating class="fa-3x" v-model="review.rating"></star-rating>
+            </div>
+            <div class="form-group">
+              <label for="content" class="text-muted"
+                >Describe your experience with
+              </label>
+              <textarea
+                name="content"
+                cols="30"
+                rows="10"
+                class="form-control"
+                v-model="review.content"
+              ></textarea>
+            </div>
+            <button
+              class="btn btn-lg- btn-primary btn-block"
+              @click.prevent="submit"
+              :disabled="loading"
             >
-            <star-rating class="fa-3x" v-model="review.rating"></star-rating>
+              Submit
+            </button>
           </div>
-          <div class="form-group">
-            <label for="content" class="text-muted"
-              >Describe your experience with
-            </label>
-            <textarea
-              name="content"
-              cols="30"
-              rows="10"
-              class="form-control"
-              v-model="review.content"
-            ></textarea>
-          </div>
-          <button class="btn btn-lg- btn-primary btn-block">Submit</button>
         </div>
       </div>
     </div>
@@ -44,40 +65,62 @@
 </template>
 
 <script>
+import { is404 } from "./../shared/utilities/response";
+
 export default {
   data() {
     return {
       review: {
+        id: null,
         rating: 5,
         content: null,
       },
       existingReview: null,
       loading: false,
       booking: null,
+      error: false,
     };
   },
   methods: {
+    submit() {
+      this.loading = true;
+      axios
+        .post(`/api/reviews`, this.review)
+        .then((response) => console.log(response))
+        .catch((err) => (this.error = true))
+        .then(() => (this.loading = false));
+    },
     onRatingChanged(rating) {
       console.log(rating);
     },
   },
   created() {
+    this.review.id = this.$route.params.id;
     this.loading = true;
 
     axios
-      .get(`/api/reviews/${this.$route.params.id}`)
-      .then( response => {
+      .get(`/api/reviews/${this.review.id}`)
+      .then((response) => {
         this.existingReview = response.data.data;
         // console.log(response.data.data);
       })
-      .catch( err => {
-        if ( err.response && err.response.status && 404 == err.response.status) {
+      .catch((err) => {
+        if (is404(err)) {
           return axios
-            .get(`/api/booking-by-review/${this.$route.params.id}`)
-            .then( response => {
+            .get(`/api/booking-by-review/${this.review.id}`)
+            .then((response) => {
               this.booking = response.data.data;
+            })
+            .catch((err) => {
+              // is404(err) ? {} : (this.error = true);
+              this.error = !is404(err);
+              // console.log("2nd " + !is404(err));
+              // if (!is404(err)) {
+              //   this.error = true;
+              // }
             });
         }
+        // this.error = true;
       })
       .then(() => {
         // console.log(this.booking.booking_id);
@@ -97,6 +140,12 @@ export default {
     hasBooking() {
       // console.log("has booking "+this.booking);
       return this.booking != null;
+    },
+    oneColumn() {
+      return !this.loading && this.alreadyReviewed;
+    },
+    twoColumns() {
+      return this.loading || !this.alreadyReviewed;
     },
   },
 };
